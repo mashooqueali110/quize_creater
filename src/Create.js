478ea62e2cf15f2513c3a3daf7e2c4ae5f470eed
Mashooque Ali <angelarader1122@gmail.com>
2021-06-12 15:30:52 +0500

    'thirdcommit'

--- src/Create.js
+++ src/Create.js
@@ -9,46 +9,44 @@ export default function Create() {
     return (
                 
 
-        <container>
-            
-        <div className="Create-page">
-        <h4> Create a Quiz  </h4>    
+      <div className="container my-1 w-10 border border-dark Create-page">
+                <h1 className='text-center'>Quiz Creater</h1>
+                <Form className="create-page-input-div">
+                    <Form.Group controlId="quizTitle">
+                        <Form.Label>Quiz Title</Form.Label>
+                        <Form.Control type="text" />
+                    </Form.Group>
+                    <Form.Group controlId="points">
+                        <Form.Label>Points</Form.Label>
+                        <Form.Control type="text" />
+                    </Form.Group>
+                    <div className="row">
+                        <div className="col">
+                            <Form.Group controlId="timeAllowed">
+                                <Form.Label>Time Allowed</Form.Label>
+                                <Form.Control type="text" />
+                            </Form.Group>
+                        </div>
+                        <div className="col">
+                            <Form.Group controlId="points">
+                                <Form.Label>Deadline</Form.Label>
+                                <Form.Control type="date" />
+                            </Form.Group>
+                        </div>
+                    </div>
+                    <h3 className='text-center'>Questions</h3>
+                    <div className="container my-4 h-50 w-80  border border-dark ">
+                        
+                        <div className="my-4 d-flex justify-content-center">
+                            <Button className='btn-primary' >Add Question</Button>
+                        </div>
+                    </div>
+                    <div className="my-3 d-flex justify-content-end">
+                        <Button className="mx-2 btn-light" variant="outline-dark"> Cancel</Button>
+                        <Button className="btn-pramary mx-2">Create Quiz</Button>
+                    </div>
+                </Form>
 
-
-        <div className="create-page-input-div">
-        <Form>
-        <Form.Group controlId="formTitle">
-    <Form.Label>Title</Form.Label>
-    <Form.Control placeholder="Enter Title Here..." />
-  </Form.Group>
-
-  <Form.Group controlId="FormPoints">
-    <Form.Label>Total Points</Form.Label>
-    <Form.Control placeholder="ex 100" />
-  </Form.Group>
-
-
-  <Form.Row  className="Create-page-input"> 
-    
-        <Form.Label> Time Alotted (minutes) </Form.Label>
-      <Form.Control placeholder="ex 10" />
-
-      
-
-  </Form.Row>
-  
-
-   </Form>
-
-        </div>
-
-            
-
-
-        </div>
-            
-
-
-        </container>
-    )
-}
+            </div>
+        )
+    }
